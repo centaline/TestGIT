31a4e9cecc5465ebb2bc78bfa442523e821f6e63
Brian Chung <brianchung@centamail.com>
2011-04-26 18:15:55 +0800

    test commit

---
+++ TestGIT/TestGITAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestGITAppDelegate.h
+//  TestGIT
+//
+//  Created by User on 26/04/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestGITAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
